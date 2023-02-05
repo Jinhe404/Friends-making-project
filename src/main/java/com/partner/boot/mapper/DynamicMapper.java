967ada9f14be2016a7c308d662c183f916f6314f
jin <3102528743@qq.com>
2023-02-05 16:22:23 +0800

    1.1.2：新增Dynamic（动态）

---
+++ src/main/java/com/partner/boot/mapper/DynamicMapper.java
@@ -0,0 +1,16 @@
+package com.partner.boot.mapper;
+
+import com.partner.boot.entity.Dynamic;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 动态	 Mapper 接口
+ * </p>
+ *
+ * @author 金同学
+ * @since 2023-02-05
+ */
+public interface DynamicMapper extends BaseMapper<Dynamic> {
+
+}
---
+++ src/main/java/com/partner/boot/service/IDynamicService.java
@@ -0,0 +1,16 @@
+package com.partner.boot.service;
+
+import com.partner.boot.entity.Dynamic;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 动态	 服务类
+ * </p>
+ *
+ * @author 金同学
+ * @since 2023-02-05
+ */
+public interface IDynamicService extends IService<Dynamic> {
+
+}
